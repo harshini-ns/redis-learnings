234cf589a695fcba1c30925b4e4c459573d48d38
harshini-ns <harshininaveen9801@gmail.com>
2025-06-05 16:08:48 +0800

    added iteration log

--- main.go
+++ main.go
@@ -30,6 +30,7 @@ func main() {
 	//push is done
 	listKey := "bikes:repairs"
 
+	client.Del(ctx, listKey)
 	dataTypes.PushToList(ctx, client, listKey, "bike:19", "bike:29", "bike:777")
 	//get elements from the push action
 	elements, err := dataTypes.GetListElements(ctx, client, listKey)
@@ -53,8 +54,32 @@ func main() {
 
 	//go routine
 	go PushNumberstoList(ctx, client, listKey)
-	//BRPOP
-	for i := 0; i < 5; i++ {
+	go BlockRpopFromList(ctx, client, listKey)
+
+	select {}
+}
+
+func PushNumberstoList(ctx context.Context, client *redis.Client, listKey string) {
+	for i := 0; i <= 5; i++ {
+		fmt.Printf("Push loop iteration %d at %s\n", i, time.Now().Format("15:04:05"))
+		length, err := client.LLen(ctx, listKey).Result()
+		if err != nil {
+			log.Printf("Error checking list length: %v", err)
+		}
+		if length == 0 {
+			fmt.Println("no items available , Pushing items to list...")
+			fmt.Println("Pushing items at", time.Now().Format("15:04:05"))
+			dataTypes.PushToList(ctx, client, listKey, "bike:10", "bike:88", "bike:90")
+
+		} else {
+			fmt.Printf("List is not empty (length: %d), skipping push.\n", length)
+		}
+		time.Sleep(3 * time.Second)
+	}
+}
+
+func BlockRpopFromList(ctx context.Context, client *redis.Client, listKey string) {
+	for i := 0; i <= 10; i++ {
 		fmt.Println("BRPOP:Waiting for element at", time.Now().Format("15:04:05"))
 
 		value, err := dataTypes.BlockRpop(ctx, client, listKey)
@@ -68,22 +93,5 @@ func main() {
 			fmt.Printf("brpop: Popped value: %s\n", value)
 
 		}
-	}
-
-}
-
-func PushNumberstoList(ctx context.Context, client *redis.Client, listKey string) {
-	for i := 0; i <= 5; i++ {
-		length, err := client.LLen(ctx, listKey).Result()
-		if err != nil {
-			log.Printf("Error checking list length: %v", err)
-		}
-		if length == 0 {
-			fmt.Println("no items available , Pushing items to list...")
-			fmt.Println("Pushing items at", time.Now().Format("15:04:05"))
-			dataTypes.PushToList(ctx, client, listKey, "bike:10", "bike:88", "bike:90")
-
-		}
-		time.Sleep(3 * time.Second)
 	}
 }
